1a66300b6276ab85fcc43ff8696145f8acc0f85d
Marco Castillo <marcocastillo2506@gmail.com>
2018-02-12 11:03:25 -0600

    Added props for description and quantity

--- client/src/components/GroceryItem.jsx
+++ client/src/components/GroceryItem.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const GroceryItem = (props) => (
   <div>
-    <span> description 1 </span>
-    <span> quantity 1 </span>
+        Item: {props.item.description}
+        Quantity: {props.item.quantity}
   </div>
 )
 
